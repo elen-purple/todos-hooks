c66c663d9af00a090c3216ad90f723eace8208af
elen-purple <anna.havrylova.2011@gmail.com>
2025-10-12 12:38:39 +0300

    a

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,47 +1,32 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Checkbox, Div, Item, Wrap, Text, Delete } from "./TodoItemStyled";
 import { IoMdCheckmark } from "react-icons/io";
 import { IoMdClose } from "react-icons/io";
 
-export class TodoItem extends Component {
-  state = {
-    checked: this.props.completed,
+export const TodoItem = ({ id, text, completed }) => {
+  const [checked, setChecked] = useState(completed);
+
+  const onConfirm = () => {
+    setChecked(!checked);
   };
 
-  onConfirm = () => {
-    this.setState((prevState) => {
-      return { checked: !prevState.checked };
-    });
-  };
-
-  render() {
-    console.log(this.state.checked);
-    return (
-      <Item id={this.props.id}>
-        <Wrap>
-          <Checkbox
-            type="checkbox"
-            data-action="checkbox"
-            checked={this.state.checked}
-            onChange={this.onConfirm}
-          />
-          <Div $checked={this.state.checked}>
-            {this.state.checked ? (
-              <IoMdCheckmark size="16" fill="#1c1c1c" />
-            ) : (
-              <></>
-            )}
-          </Div>
-        </Wrap>
-        <Text checked={this.state.checked}>{this.props.text}</Text>
-        <Delete type="button" data-action="delete">
-          <IoMdClose
-            size="16"
-            fill="#1c1c1c"
-            style={{ pointerEvents: "none" }}
-          />
-        </Delete>
-      </Item>
-    );
-  }
-}
+  return (
+    <Item id={id}>
+      <Wrap>
+        <Checkbox
+          type="checkbox"
+          data-action="checkbox"
+          checked={checked}
+          onChange={onConfirm}
+        />
+        <Div $checked={checked}>
+          {checked ? <IoMdCheckmark size="16" fill="#1c1c1c" /> : <></>}
+        </Div>
+      </Wrap>
+      <Text checked={checked}>{text}</Text>
+      <Delete type="button" data-action="delete">
+        <IoMdClose size="16" fill="#1c1c1c" style={{ pointerEvents: "none" }} />
+      </Delete>
+    </Item>
+  );
+};
